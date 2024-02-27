c579680d6d49d7902250942428c48fccdd830e2f
mobin-newroz <ahmobin@newroztech.com>
2024-02-27 12:32:43 +0600

    Category component

--- src/sidebar/category/Category.jsx
+++ src/sidebar/category/Category.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 import CategoryStyle from './category.module.css';
 function Category() {
     return (
-        <div></div>
+        <div>
+            <h2 className={CategoryStyle.sidebarTitle}>Category</h2>
+
+            <div>
+                <label className={CategoryStyle.sidebarLabelContainer}>
+                    <input type="radio" name="test"/>
+                    <span className={CategoryStyle.checkmark}></span> All
+                </label>
+
+                <label className={CategoryStyle.sidebarLabelContainer}>
+                    <input type="radio" name="test"/>
+                    <span className={CategoryStyle.checkmark}></span> Sneakers
+                </label>
+
+                <label className={CategoryStyle.sidebarLabelContainer}>
+                    <input type="radio" name="test"/>
+                    <span className={CategoryStyle.checkmark}></span> Flats
+                </label>
+
+                <label className={CategoryStyle.sidebarLabelContainer}>
+                    <input type="radio" name="test"/>
+                    <span className={CategoryStyle.checkmark}></span> Sandals
+                </label>
+
+                <label className={CategoryStyle.sidebarLabelContainer}>
+                    <input type="radio" name="test"/>
+                    <span className={CategoryStyle.checkmark}></span> Heels
+                </label>
+            </div>
+        </div>
     );
 }
 
