5928a3e4a39bdb3e13af00dd1470aa96336760cc
mobin-newroz <ahmobin@newroztech.com>
2024-02-27 11:55:01 +0600

    Recommended component

--- src/recommended/Recommended.jsx
+++ src/recommended/Recommended.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import RecomStyle from './recommended.module.css';
 function Recommended() {
     return (
-        <div>Recommended</div>
+        <>
+            <h2 className={RecomStyle.recommendedTitle}>Recommended</h2>
+            <div className={RecomStyle.recommendedFlex}>
+                <button className={RecomStyle.recommendedBtn}>All Products</button>
+                <button className={RecomStyle.recommendedBtn}>Nike</button>
+                <button className={RecomStyle.recommendedBtn}>Adidas</button>
+                <button className={RecomStyle.recommendedBtn}>Puma</button>
+                <button className={RecomStyle.recommendedBtn}>Vans</button>
+            </div>
+        </>
     );
 }
 
